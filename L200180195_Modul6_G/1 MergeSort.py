acccd8b2b6f07ffb1e08b55552ac16f1303e2e56
l200180195 <48119186+l200180195@users.noreply.github.com>
2020-04-03 19:45:24 +0700

    Add files via upload

---
+++ L200180195_Modul6_G/1 MergeSort.py
@@ -0,0 +1,78 @@
+class MhsTIF():
+    def __init__(self, nim):
+        self.nim = nim
+
+    def __str__(self):
+        return str(self.nim)
+    
+m0 = MhsTIF(10)
+m1 = MhsTIF(51)
+m2 = MhsTIF(2)
+m3 = MhsTIF(18)
+m4 = MhsTIF(4)
+m5 = MhsTIF(31)
+m6 = MhsTIF(13)
+m7 = MhsTIF(5)
+m8 = MhsTIF(23)
+m9 = MhsTIF(64)
+m10 = MhsTIF(29)
+
+m0.next = m1
+m1.next = m2
+m2.next = m3
+m3.next = m4
+m4.next = m5
+m5.next = m6
+m6.next = m7
+m7.next = m8
+m8.next = m9
+m9.next = m10
+
+def mergeSort(A):
+    #print("Membelah      ",A)
+    if len(A) > 1:
+        mid = len(A) // 2
+        separuhkiri = A[:mid]
+        separuhkanan = A[mid:]
+
+        mergeSort(separuhkiri)
+        mergeSort(separuhkanan)
+
+        i = 0;j=0;k=0
+        while i < len(separuhkiri) and j < len(separuhkanan):
+            if separuhkiri[i] < separuhkanan[j]:
+                A[k] = separuhkiri[i]
+                i = i + 1
+            else:
+                A[k] = separuhkanan[j]
+                j = j + 1
+            k=k+1
+
+        while i < len(separuhkiri):
+            A[k] = separuhkiri[i]
+            i = i + 1
+            k=k+1
+
+        while j < len(separuhkanan):
+            A[k] = separuhkanan[j]
+            j = j + 1
+            k=k+1
+    #print("Menggabungkan",A)
+
+def convert(arr, obj):
+    hasil=[]
+    for x in range (len(arr)):
+        for i in range (len(arr)):
+            if arr[x] == obj[i].nim:
+                hasil.append(obj[i])
+    return hasil
+
+Daftar = [m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10]
+A = []
+for x in Daftar:
+    A.append(x.nim)
+
+print("MERGE SORT")
+mergeSort(A)
+for x in convert(A, Daftar):
+    print (x.nim)
